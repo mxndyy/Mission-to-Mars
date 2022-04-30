b6cec1de64225dba4bccda2c4dde0b1d32064eae
mxndyy <amandakj@uci.edu>
2022-04-29 20:04:27 -0700

    Mission-to-Mars

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
 import scraping
 
@@ -19,8 +19,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
-
-mars.update_one({}, {"$set":mars_data}, upsert=True)
 
 if __name__ == "__main__":
     app.run()
